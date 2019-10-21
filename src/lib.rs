3967afbd255bceba053bfa6624a013a08446c643
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-21 13:32:41 -0700

    lib: Worker structure to encapsulate the twitter_stream instance

--- src/lib.rs
+++ src/lib.rs
@@ -13,18 +13,32 @@ impl Tracker {
     }
     pub fn run(&self) {
         for track in &self.config.tracks {
-            use twitter_stream::rt::{self, Future, Stream};
-            use twitter_stream::TwitterStreamBuilder;
-            let future = TwitterStreamBuilder::filter(&self.config.token)
-                .track(Some(*track))
-                .listen()
-                .flatten_stream()
-                .for_each(|json| {
-                    println!("{}", json);
-                    Ok(())
-                })
-                .map_err(|e| println!("error: {}", e));
-            rt::run(future);
+            Worker::new(&self.config, track).run();
         }
+    }
+}
+
+pub struct Worker<'a> {
+    config: &'a config::Config,
+    track: &'a str,
+}
+
+impl<'a> Worker<'a> {
+    pub fn new(config: &'a config::Config, track: &'a str) -> Worker<'a> {
+        Worker { config, track }
+    }
+    pub fn run(&self) {
+        use twitter_stream::rt::{self, Future, Stream};
+        use twitter_stream::TwitterStreamBuilder;
+        let future = TwitterStreamBuilder::filter(&self.config.token)
+            .track(Some(self.track))
+            .listen()
+            .flatten_stream()
+            .for_each(|json| {
+                println!("{}", json);
+                Ok(())
+            })
+            .map_err(|e| println!("error: {}", e));
+        rt::run(future);
     }
 }
