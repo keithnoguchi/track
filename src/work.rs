0300773288f5ecf0e21a6d43b58be68ebc2267f2
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-21 14:17:30 -0700

    work: Refer Config through super::Config

--- src/work.rs
+++ src/work.rs
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
 extern crate twitter_stream;
 
-use super::config;
-
 pub struct Worker<'a> {
-    config: &'a config::Config,
+    config: &'a super::Config,
     track: &'a str,
 }
 
 impl<'a> Worker<'a> {
-    pub fn new(config: &'a config::Config, track: &'a str) -> Worker<'a> {
+    pub fn new(config: &'a super::Config, track: &'a str) -> Worker<'a> {
         Worker { config, track }
     }
     pub fn run(&self) {
