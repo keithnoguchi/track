06a68570dedf45ca873d8a5e7f45ba5e6e5df0c4
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-21 14:38:50 -0700

    config: Add the first unit test for track!
    Woohoo! :)

--- src/config.rs
+++ src/config.rs
@@ -8,10 +8,10 @@ impl Config {
     pub fn new() -> Config {
         use std::env;
         use twitter_stream::Token;
-        let ckey = env::var("TRACK_CONSUMER_KEY").unwrap();
-        let csecret = env::var("TRACK_CONSUMER_SECRET").unwrap();
-        let atoken = env::var("TRACK_ACCESS_TOKEN").unwrap();
-        let asecret = env::var("TRACK_ACCESS_SECRET").unwrap();
+        let ckey = env::var("TRACK_CONSUMER_KEY").unwrap_or("dummy".to_string());
+        let csecret = env::var("TRACK_CONSUMER_SECRET").unwrap_or("dummy".to_string());
+        let atoken = env::var("TRACK_ACCESS_TOKEN").unwrap_or("dummy".to_string());
+        let asecret = env::var("TRACK_ACCESS_SECRET").unwrap_or("dummy".to_string());
         let token = Token::new(ckey, csecret, atoken, asecret);
         let tracks = vec![
             "twitter",
@@ -26,5 +26,26 @@ impl Config {
             "food",
         ];
         Config { token, tracks }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn default_track_entries() {
+        let want = vec![
+            "twitter",
+            "facebook",
+            "google",
+            "travel",
+            "art",
+            "music",
+            "photography",
+            "love",
+            "fashion",
+            "food",
+        ];
+        let config = super::Config::new();
+        assert_eq!(want, config.tracks);
     }
 }
