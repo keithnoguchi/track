f78006a0875ad5a3e08fa042a28cd4ad650bfa3a
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-21 11:12:29 -0700

    lib: Move Config to the track crate

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0
+extern crate twitter_stream;
+
+pub struct Config {
+    pub token: twitter_stream::Token,
+}
+
+impl Config {
+    pub fn new() -> Config {
+        use std::env;
+        use twitter_stream::Token;
+        let ckey = env::var("TRACK_CONSUMER_KEY").unwrap();
+        let csecret = env::var("TRACK_CONSUMER_SECRET").unwrap();
+        let atoken = env::var("TRACK_ACCESS_TOKEN").unwrap();
+        let asecret = env::var("TRACK_ACCESS_SECRET").unwrap();
+        let token = Token::new(ckey, csecret, atoken, asecret);
+        Config { token }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
+extern crate track;
 extern crate twitter_stream;
 
 use twitter_stream::rt::{self, Future, Stream};
-use twitter_stream::{Token, TwitterStreamBuilder};
+use twitter_stream::TwitterStreamBuilder;
 
 fn main() {
-    let c = Config::new();
+    let c = track::Config::new();
     let future = TwitterStreamBuilder::filter(&c.token)
         .track(Some("twitter"))
         .listen()
@@ -17,20 +18,4 @@ fn main() {
         .map_err(|e| println!("error: {}", e));
 
     rt::run(future);
-}
-
-struct Config {
-    token: Token,
-}
-
-impl Config {
-    fn new() -> Config {
-        use std::env;
-        let ckey = env::var("TRACK_CONSUMER_KEY").unwrap();
-        let csecret = env::var("TRACK_CONSUMER_SECRET").unwrap();
-        let atoken = env::var("TRACK_ACCESS_TOKEN").unwrap();
-        let asecret = env::var("TRACK_ACCESS_SECRET").unwrap();
-        let token = Token::new(ckey, csecret, atoken, asecret);
-        Config { token }
-    }
 }
