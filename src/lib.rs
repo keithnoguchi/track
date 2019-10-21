a124ef901dde265df80db16878ad288fdabe6015
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-21 13:38:17 -0700

    work: Worker thread module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 extern crate twitter_stream;
 
 pub mod config;
+pub mod work;
 
 pub struct Tracker {
     config: config::Config,
@@ -13,32 +14,7 @@ impl Tracker {
     }
     pub fn run(&self) {
         for track in &self.config.tracks {
-            Worker::new(&self.config, track).run();
+            work::Worker::new(&self.config, track).run();
         }
-    }
-}
-
-pub struct Worker<'a> {
-    config: &'a config::Config,
-    track: &'a str,
-}
-
-impl<'a> Worker<'a> {
-    pub fn new(config: &'a config::Config, track: &'a str) -> Worker<'a> {
-        Worker { config, track }
-    }
-    pub fn run(&self) {
-        use twitter_stream::rt::{self, Future, Stream};
-        use twitter_stream::TwitterStreamBuilder;
-        let future = TwitterStreamBuilder::filter(&self.config.token)
-            .track(Some(self.track))
-            .listen()
-            .flatten_stream()
-            .for_each(|json| {
-                println!("{}", json);
-                Ok(())
-            })
-            .map_err(|e| println!("error: {}", e));
-        rt::run(future);
     }
 }
---
+++ src/work.rs
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0
+use super::config;
+
+pub struct Worker<'a> {
+    config: &'a config::Config,
+    track: &'a str,
+}
+
+impl<'a> Worker<'a> {
+    pub fn new(config: &'a config::Config, track: &'a str) -> Worker<'a> {
+        Worker { config, track }
+    }
+    pub fn run(&self) {
+        use twitter_stream::rt::{self, Future, Stream};
+        use twitter_stream::TwitterStreamBuilder;
+        let future = TwitterStreamBuilder::filter(&self.config.token)
+            .track(Some(self.track))
+            .listen()
+            .flatten_stream()
+            .for_each(|json| {
+                println!("{}", json);
+                Ok(())
+            })
+            .map_err(|e| println!("error: {}", e));
+        rt::run(future);
+    }
+}
