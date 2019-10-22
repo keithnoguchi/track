ed7f996d217196274c71e4cfdf968212fd99797e
Keith Noguchi <keith.noguchi@gmail.com>
2019-10-22 12:31:15 -0700

    event: Fix Event's Eq and PartialEq trates implementation

--- src/event.rs
+++ src/event.rs
@@ -2,7 +2,7 @@
 use std::fmt;
 use std::str;
 
-#[derive(Hash, Clone, Copy, Debug)]
+#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
 pub enum Event {
     Twitter,
     Facebook,
@@ -16,13 +16,6 @@ pub enum Event {
     Food,
     Other,
 }
-
-impl PartialEq for Event {
-    fn eq(&self, other: &Event) -> bool {
-        self == other
-    }
-}
-impl Eq for Event {}
 
 impl fmt::Display for Event {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -57,6 +50,84 @@ impl str::FromStr for Event {
             "fashion" => Ok(Event::Fashion),
             "food" => Ok(Event::Food),
             _ => Ok(Event::Other),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Event;
+    #[test]
+    fn from_str() {
+        struct Test {
+            name: &'static str,
+            data: &'static str,
+            want: Result<Event, ()>,
+        }
+        let tests = [
+            Test {
+                name: "twitter trend",
+                data: "twitter",
+                want: Ok(Event::Twitter),
+            },
+            Test {
+                name: "facebook trend",
+                data: "facebook",
+                want: Ok(Event::Facebook),
+            },
+            Test {
+                name: "google trend",
+                data: "google",
+                want: Ok(Event::Google),
+            },
+            Test {
+                name: "travel trend",
+                data: "travel",
+                want: Ok(Event::Travel),
+            },
+            Test {
+                name: "art trend",
+                data: "art",
+                want: Ok(Event::Art),
+            },
+            Test {
+                name: "music trend",
+                data: "music",
+                want: Ok(Event::Music),
+            },
+            Test {
+                name: "photography trend",
+                data: "photography",
+                want: Ok(Event::Photography),
+            },
+            Test {
+                name: "love trend",
+                data: "love",
+                want: Ok(Event::Love),
+            },
+            Test {
+                name: "fashion trend",
+                data: "fashion",
+                want: Ok(Event::Fashion),
+            },
+            Test {
+                name: "food trend",
+                data: "food",
+                want: Ok(Event::Food),
+            },
+            Test {
+                name: "other trend",
+                data: "other",
+                want: Ok(Event::Other),
+            },
+            Test {
+                name: "another trend",
+                data: "another",
+                want: Ok(Event::Other),
+            },
+        ];
+        for t in &tests {
+            debug_assert_eq!(t.want, t.data.parse::<Event>(), "{}", t.name);
         }
     }
 }
